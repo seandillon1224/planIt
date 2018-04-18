5bc5e4196f35fbf6bd4e88f46ec10aabdc800f60
Sean Dillon <seandillon1224@gmail.com>
2018-04-18 17:39:30 -0400

    Enter

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ var multer = require("multer");
 var upload = multer({storage: multer.memoryStorage({})});
 var fs = require("fs");
 
-//requirements for socket messaging
- const SocketManager = require('./client/src/SocketManager')
-
- io.on('connection', SocketManager)
 
 // connect to the database and load models
 
